4d41f2c0692a27a1880beb24926f270fbc3744de
igordevnet <igorsouzaalmeida2404@gmail.com>
2026-08-30 13:42:17 -0300

    Feat: Building the http scanner so as to Scan all controllers and Http annotations within the target folder

--- src/main/java/com/concurium/Main.java
+++ src/main/java/com/concurium/Main.java
@@ -2,6 +2,7 @@ package com.concurium;
 
 import com.concurium.annotations.Controller;
 import com.concurium.annotations.Post;
+import com.concurium.bootstrap.ConcuriumApplication;
 import com.concurium.server.ConcServlet;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
@@ -15,18 +16,20 @@ public class Main {
     private static final int serverPort = 8080;
 
     public static void main(String[] args) throws LifecycleException {
-        Tomcat tomcatServer = new Tomcat();
+//        Tomcat tomcatServer = new Tomcat();
+//
+//        tomcatServer.setPort(serverPort);
+//        tomcatServer.getConnector();
+//        tomcatServer.setBaseDir(new File(".").getAbsolutePath());
+//
+//        var context = tomcatServer.addContext("", new File(".").getAbsolutePath());
+//        Wrapper concServlet = tomcatServer.addServlet(context, "ConcServlet", new ConcServlet());
+//        context.addServletMappingDecoded("/hello", "ConcServlet");
 
-        tomcatServer.setPort(serverPort);
-        tomcatServer.getConnector();
-        tomcatServer.setBaseDir(new File(".").getAbsolutePath());
-
-        var context = tomcatServer.addContext("", new File(".").getAbsolutePath());
-        Wrapper concServlet = tomcatServer.addServlet(context, "ConcServlet", new ConcServlet());
-        context.addServletMappingDecoded("/hello", "ConcServlet");
-
-        tomcatServer.start();
-        tomcatServer.getServer().await();
+        ConcuriumApplication.run(Main.class);
+//
+//        tomcatServer.start();
+//        tomcatServer.getServer().await();
 
     }
 }
--- src/main/java/com/concurium/bootstrap/ConcuriumApplication.java
+++ src/main/java/com/concurium/bootstrap/ConcuriumApplication.java
@@ -1,39 +1,67 @@
 package com.concurium.bootstrap;
 
-import com.concurium.annotations.Controller;
-import com.concurium.annotations.Get;
+import com.concurium.annotations.*;
 import org.reflections.Reflections;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class ConcuriumApplication {
 
+    public record RouteTarget(Object controllerInstance, Method method) {}
+
+    private static final List<Class<? extends Annotation>> HTTP_VERBS = List.of(
+            Get.class, Post.class, Put.class, Delete.class, Patch.class, Query.class
+    );
+
     public static void run(Class<?> mainClass) {
         String appPackage = mainClass.getPackageName();
         var reflection = new Reflections(appPackage);
-        Set<Class<?>> controllerClasses = reflection.getTypesAnnotatedWith(Controller.class);
-        Map<String, Method> routePaths = new HashMap<>();
 
-        controllerClasses.forEach(clazz -> {
+        var routes = httpScanner(appPackage, reflection);
+    }
+
+    private static Map<String, RouteTarget> httpScanner(String appPackage, Reflections reflection) {
+        Set<Class<?>> controllerClasses = reflection.getTypesAnnotatedWith(Controller.class);
+
+        Map<String, RouteTarget> routeRegistry = new HashMap<>();
+
+        for (Class<?> clazz : controllerClasses) {
             try {
-                Object instance = clazz.getDeclaredConstructor().newInstance();
-                for(Method method : clazz.getDeclaredMethods()) {
-                    if(method.isAnnotationPresent(Get.class)){
-                        var path = method.getAnnotation(Get.class);
-                        routePaths.computeIfAbsent(path.value(), key -> method);
+                Object controllerInstance = clazz.getDeclaredConstructor().newInstance();
+                Controller controllerAnnotation = clazz.getAnnotation(Controller.class);
+                String basePath = controllerAnnotation.value();
+
+                for (Method method : clazz.getDeclaredMethods()) {
+                    for (Class<? extends Annotation> verbClass : HTTP_VERBS) {
+                        if (method.isAnnotationPresent(verbClass)) {
+                            Annotation annotation = method.getAnnotation(verbClass);
+                            String methodPath = (String) verbClass.getMethod("value").invoke(annotation);
+
+                            String fullPath = basePath + methodPath;
+
+                            fullPath = fullPath.replaceAll("//+", "/");
+
+                            String httpMethod = verbClass.getSimpleName().toUpperCase();
+                            String routeKey = httpMethod + " " + fullPath;
+
+                            routeRegistry.put(routeKey, new RouteTarget(controllerInstance, method));
+                        }
                     }
                 }
-            } catch (InstantiationException |
-                     IllegalAccessException |
-                     InvocationTargetException |
-                     NoSuchMethodException e
-            ) {
-                throw new RuntimeException(e);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to initialize controllers", e);
             }
-        });
+        }
+
+        routeRegistry.forEach((key, target) ->
+                System.out.println("Mapped: " + key + " -> " + target.controllerInstance().getClass().getSimpleName() + "." + target.method().getName())
+        );
+
+        return routeRegistry;
     }
 }
